36e60f448e972cc2edc3b3509a20362e138f1c49
Jacob Hash <jhash@umich.edu>
2015-10-11 23:13:33 -0700

    Fixed safari sizing issues with onImageLoad

--- lib/lightBox.jsx
+++ lib/lightBox.jsx
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React from 'react';
 import Masonry from 'masonry-layout';
 import {Image} from './image.jsx!';
@@ -16,19 +17,23 @@ export var LightBox = React.createClass({
       }
     };
   },
-  onImageLoad: function() {
+  onImageLoad: _.debounce(function() {
+    if (this.state.wasTooWide || !this.refs.image || !this.refs.lightBoxContainer) {
+      this.setState({ wasTooWide: false });
+      return;
+    }
+
     var tooWide = this.refs.image.getDOMNode().offsetWidth > this.refs.lightBoxContainer.getDOMNode().offsetWidth;
 
     this.setState({
+      wasTooWide: tooWide,
       loading: false,
       imageStyles: {
         height: tooWide ? 'auto' : '100%',
         width: tooWide ? '100%' : 'auto'
       }
     });
-
-    window.addEventListener('resize', this.onImageLoad);
-  },
+  }, 100),
   onClick: function() {
     this.props.closeLightBox();
   },
@@ -36,9 +41,13 @@ export var LightBox = React.createClass({
     window.removeEventListener('resize', this.onImageLoad);
   },
   render: function() {
-    return <div ref='lightBoxContainer' className='light-box' onClick={this.onClick}>
+    var lightBoxContainer = (<div ref='lightBoxContainer' className='light-box' onClick={this.onClick}>
       <Image ref='image' onLoad={this.onImageLoad} src={this.props.img.link} alt={this.props.img.alt} styles={this.state.imageStyles} widths={this.props.img.widths} />
       { this.state.loading ? <div ref='spinner' className='spinner'></div> : null }
-    </div>;
+    </div>);
+
+    window.addEventListener('resize', this.onImageLoad);
+
+    return lightBoxContainer;
   }
 });
